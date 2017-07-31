629f983a3e1bed3401bf940f97b8e8a9db4f5e96
Marcel Pociot <m.pociot@gmail.com>
2017-07-31 14:28:02 +0200

    Initial commit

---
+++ src/WebDriver.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace BotMan\Drivers\Web;
+
+use BotMan\BotMan\Users\User;
+use Illuminate\Support\Collection;
+use BotMan\BotMan\Drivers\HttpDriver;
+use BotMan\BotMan\Interfaces\WebAccess;
+use BotMan\BotMan\Messages\Incoming\Answer;
+use Symfony\Component\HttpFoundation\Request;
+use BotMan\BotMan\Messages\Outgoing\Question;
+use Symfony\Component\HttpFoundation\Response;
+use BotMan\BotMan\Messages\Incoming\IncomingMessage;
+use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
+
+class WebDriver extends HttpDriver
+{
+    const DRIVER_NAME = 'Web';
+
+    /** @var OutgoingMessage[] */
+    protected $replies = [];
+
+    /** @var int */
+    protected $replyStatusCode = 200;
+
+    /** @var string */
+    protected $errorMessage = '';
+
+    /**
+     * @param Request $request
+     */
+    public function buildPayload(Request $request)
+    {
+        $this->payload = $request->request->all();
+        $this->event = Collection::make($this->payload);
+    }
+
+    /**
+     * @param IncomingMessage $matchingMessage
+     * @return \BotMan\BotMan\Users\User
+     */
+    public function getUser(IncomingMessage $matchingMessage)
+    {
+        return new User($matchingMessage->getSender());
+    }
+
+    /**
+     * Determine if the request is for this driver.
+     *
+     * @return bool
+     */
+    public function matchesRequest()
+    {
+        return $this->event->get('driver') === 'web';
+    }
+
+    /**
+     * @param  IncomingMessage $message
+     * @return \BotMan\BotMan\Messages\Incoming\Answer
+     */
+    public function getConversationAnswer(IncomingMessage $message)
+    {
+        return Answer::create($message->getText())->setMessage($message);
+    }
+
+    /**
+     * Retrieve the chat message.
+     *
+     * @return array
+     */
+    public function getMessages()
+    {
+        $message = $this->event->get('message');
+        $userId = $this->event->get('userId');
+
+        return [new IncomingMessage($message, $userId, $userId, $this->payload)];
+    }
+
+    /**
+     * @return bool
+     */
+    public function isBot()
+    {
+        return false;
+    }
+
+    /**
+     * @param string|Question|OutgoingMessage $message
+     * @param IncomingMessage $matchingMessage
+     * @param array $additionalParameters
+     * @return Response
+     */
+    public function buildServicePayload($message, $matchingMessage, $additionalParameters = [])
+    {
+        if (! $message instanceof WebAccess && ! $message instanceof OutgoingMessage) {
+            $this->errorMessage = 'Unsupported message type.';
+            $this->replyStatusCode = 500;
+        }
+
+        return $message;
+    }
+
+    /**
+     * @param mixed $payload
+     * @return Response
+     */
+    public function sendPayload($payload)
+    {
+        $this->replies[] = $payload;
+    }
+
+    /**
+     * @param $messages
+     * @return array
+     */
+    protected function buildReply($messages) {
+        $replyData = Collection::make($messages)->transform(function ($message) {
+            $reply = [];
+
+            if ($message instanceof WebAccess) {
+                $reply = $message->toWebDriver();
+            } elseif ($message instanceof OutgoingMessage) {
+                $reply = [
+                    'type' => 'text',
+                    'text' => $message->getText(),
+                ];
+            }
+
+            return $reply;
+        })->toArray();
+
+        return $replyData;
+    }
+
+    /**
+     * Send out message response.
+     */
+    public function messagesHandled()
+    {
+        $messages = $this->buildReply($this->replies);
+
+        // Reset replies
+        $this->replies = [];
+
+        Response::create(json_encode([
+            'status' => $this->replyStatusCode,
+            'messages' => $messages,
+        ]), $this->replyStatusCode, [
+            'Content-Type' => 'application/json',
+            'Access-Control-Allow-Credentials' => true,
+            'Access-Control-Allow-Origin' => '*',
+        ])->send();
+    }
+
+    /**
+     * @return bool
+     */
+    public function isConfigured()
+    {
+        return false;
+    }
+
+    /**
+     * Low-level method to perform driver specific API requests.
+     *
+     * @param string $endpoint
+     * @param array $parameters
+     * @param \BotMan\BotMan\Messages\Incoming\IncomingMessage $matchingMessage
+     * @return void
+     */
+    public function sendRequest($endpoint, array $parameters, IncomingMessage $matchingMessage)
+    {
+        // Not available with the web driver.
+    }
+}
---
+++ tests/WebDriverTest.php
@@ -0,0 +1,271 @@
+<?php
+
+namespace Tests;
+
+use BotMan\BotMan\Http\Curl;
+use BotMan\BotMan\Messages\Incoming\IncomingMessage;
+use BotMan\BotMan\Messages\Outgoing\Actions\Button;
+use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
+use BotMan\BotMan\Messages\Outgoing\Question;
+use BotMan\Drivers\Facebook\Extensions\ButtonTemplate;
+use BotMan\Drivers\Facebook\Extensions\Element;
+use BotMan\Drivers\Facebook\Extensions\ElementButton;
+use BotMan\Drivers\Facebook\Extensions\GenericTemplate;
+use BotMan\Drivers\Facebook\Extensions\ListTemplate;
+use BotMan\Drivers\Facebook\Extensions\ReceiptAddress;
+use BotMan\Drivers\Facebook\Extensions\ReceiptAdjustment;
+use BotMan\Drivers\Facebook\Extensions\ReceiptElement;
+use BotMan\Drivers\Facebook\Extensions\ReceiptSummary;
+use BotMan\Drivers\Facebook\Extensions\ReceiptTemplate;
+use BotMan\Drivers\Web\WebDriver;
+use Mockery as m;
+use PHPUnit_Framework_TestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class WebDriverTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @param $responseData
+     * @param null $htmlInterface
+     * @return WebDriver
+     */
+    private function getDriver($responseData, $htmlInterface = null)
+    {
+        $request = Request::create('', 'POST', $responseData);
+        if ($htmlInterface === null) {
+            $htmlInterface = m::mock(Curl::class);
+        }
+
+        return new WebDriver($request, [], $htmlInterface);
+    }
+
+    /** @test */
+    public function it_returns_the_driver_name()
+    {
+        $driver = $this->getDriver([]);
+        $this->assertSame('Web', $driver->getName());
+    }
+
+    /** @test */
+    public function it_matches_the_request()
+    {
+        $driver = $this->getDriver([
+            'driver' => 'api',
+            'message' => 'Hi Julia',
+            'userId' => '12345',
+        ]);
+        $this->assertFalse($driver->matchesRequest());
+
+        $driver = $this->getDriver([
+            'driver' => 'web',
+            'message' => 'Hi Julia',
+            'userId' => '12345',
+        ]);
+        $this->assertTrue($driver->matchesRequest());
+    }
+
+    /** @test */
+    public function it_returns_the_message_object()
+    {
+        $driver = $this->getDriver([
+            'driver' => 'web',
+            'message' => 'Hi Julia',
+            'userId' => '12345',
+        ]);
+        $this->assertTrue(is_array($driver->getMessages()));
+    }
+
+    /** @test */
+    public function it_returns_the_message_text()
+    {
+        $driver = $this->getDriver([
+            'driver' => 'web',
+            'message' => 'Hi Julia',
+            'userId' => '12345',
+        ]);
+        $this->assertSame('Hi Julia', $driver->getMessages()[0]->getText());
+    }
+
+    /** @test */
+    public function it_returns_the_user_id()
+    {
+        $driver = $this->getDriver([
+            'driver' => 'web',
+            'message' => 'Hi Julia',
+            'userId' => '12345',
+        ]);
+        $this->assertSame('12345', $driver->getMessages()[0]->getSender());
+    }
+
+    /** @test */
+    public function it_can_reply_string_messages()
+    {
+        $driver = $this->getDriver([
+            'driver' => 'web',
+            'message' => 'Hi Julia',
+            'userId' => '12345',
+        ]);
+
+        $message = new IncomingMessage('', '', '1234567890');
+
+        $payload = $driver->buildServicePayload(new OutgoingMessage('Test one From API'), $message);
+        $driver->sendPayload($payload);
+        $payload = $driver->buildServicePayload(new OutgoingMessage('Test two From API'), $message);
+        $driver->sendPayload($payload);
+        $driver->messagesHandled();
+
+        $this->expectOutputString('{"status":200,"messages":[{"type":"text","text":"Test one From API"},{"type":"text","text":"Test two From API"}]}');
+    }
+
+    /**
+     * @test
+     **/
+    public function it_replies_to_question_object()
+    {
+        $driver = $this->getDriver([
+            'driver' => 'web',
+            'message' => 'Hi Julia',
+            'userId' => '12345',
+        ]);
+
+        $message = new IncomingMessage('', '', '1234567890');
+        $question = Question::create('What do want to do?')
+            ->addButton(Button::create('Stay')->image('https://test.com/image.png')->value('stay'))
+            ->addButton(Button::create('Leave'));
+        $payload = $driver->buildServicePayload($question, $message);
+        $driver->sendPayload($payload);
+        $driver->messagesHandled();
+
+        $json = $question->toWebDriver();
+        $this->expectOutputString('{"status":200,"messages":['.json_encode($json).']}');
+    }
+
+    /**
+     * @test
+     **/
+    public function it_replies_to_facebook_button_template()
+    {
+        $driver = $this->getDriver([
+            'driver' => 'web',
+            'message' => 'Hi Julia',
+            'userId' => '12345',
+        ]);
+
+        $message = new IncomingMessage('', '', '1234567890');
+        $template = ButtonTemplate::create('How do you like BotMan so far?')
+            ->addButton(ElementButton::create('Quite good')->type('postback')->payload('good'))
+            ->addButton(ElementButton::create('Love it!')->url('https://test.at'));
+
+        $payload = $driver->buildServicePayload($template, $message);
+        $driver->sendPayload($payload);
+        $driver->messagesHandled();
+
+        $json = $template->toWebDriver();
+        $this->expectOutputString('{"status":200,"messages":['.json_encode($json).']}');
+    }
+
+    /**
+     * @test
+     **/
+    public function it_replies_to_facebook_list_template()
+    {
+        $driver = $this->getDriver([
+            'driver' => 'web',
+            'message' => 'Hi Julia',
+            'userId' => '12345',
+        ]);
+
+        $message = new IncomingMessage('', '', '1234567890');
+        $template = ListTemplate::create()
+            ->useCompactView()
+            ->addGlobalButton(
+                ElementButton::create('view more')
+                    ->url('http://test.at'))
+            ->addElement(
+                Element::create('BotMan Documentation')
+                    ->subtitle('All about BotMan')
+                    ->image('http://botman.io/img/botman-body.png')
+                    ->addButton(ElementButton::create('tell me more')->payload('tellmemore')->type('postback')))
+            ->addElement(
+                Element::create('BotMan Laravel Starter')
+                    ->image('http://botman.io/img/botman-body.png')
+                    ->addButton(ElementButton::create('visit')->url('https://github.com/mpociot/botman-laravel-starter'))
+            );
+
+        $payload = $driver->buildServicePayload($template, $message);
+        $driver->sendPayload($payload);
+        $driver->messagesHandled();
+
+        $json = $template->toWebDriver();
+        $this->expectOutputString('{"status":200,"messages":['.json_encode($json).']}');
+    }
+
+    /**
+     * @test
+     **/
+    public function it_replies_to_facebook_generic_template()
+    {
+        $driver = $this->getDriver([
+            'driver' => 'web',
+            'message' => 'Hi Julia',
+            'userId' => '12345',
+        ]);
+
+        $message = new IncomingMessage('', '', '1234567890');
+        $template = GenericTemplate::create()
+            ->addElements([
+                Element::create('BotMan Documentation')
+                    ->itemUrl('http://botman.io/')
+                    ->image('http://screenshots.nomoreencore.com/botman2.png')
+                    ->subtitle('All about BotMan')
+                    ->addButton(ElementButton::create('visit')->url('http://botman1.io'))
+                    ->addButton(ElementButton::create('tell me more')->payload('tellmemore')->type('postback')),
+                Element::create('BotMan Laravel Starter')
+                    ->itemUrl('https://github.com/mpociot/botman-laravel-starter')
+                    ->image('http://screenshots.nomoreencore.com/botman.png')
+                    ->subtitle('This is the best way to start with Laravel and BotMan')
+                    ->addButton(ElementButton::create('visit')->url('https://github.com/mpociot/botman-laravel-starter')),
+            ]);
+
+        $payload = $driver->buildServicePayload($template, $message);
+        $driver->sendPayload($payload);
+        $driver->messagesHandled();
+
+        $json = $template->toWebDriver();
+        $this->expectOutputString('{"status":200,"messages":['.json_encode($json).']}');
+    }
+
+    /**
+     * @test
+     **/
+    public function it_replies_to_facebook_receipt_template()
+    {
+        $driver = $this->getDriver([
+            'driver' => 'web',
+            'message' => 'Hi Julia',
+            'userId' => '12345',
+        ]);
+
+        $message = new IncomingMessage('', '', '1234567890');
+        $template = ReceiptTemplate::create()
+            ->recipientName('Christoph Rumpel')
+            ->merchantName('BotMan GmbH')
+            ->orderNumber('342343434343')
+            ->timestamp('1428444852')
+            ->orderUrl('http://test.at')
+            ->currency('USD')
+            ->paymentMethod('VISA')
+            ->addElement(ReceiptElement::create('T-Shirt Small')->price(15.99)->image('http://botman.io/img/botman-body.png')->quantity(2)->subtitle('v1')->currency('USD'))
+            ->addElement(ReceiptElement::create('Sticker')->price(2.99)->image('http://botman.io/img/botman-body.png')->subtitle('Logo 1')->currency('USD'))
+            ->addAddress(ReceiptAddress::create()->street1('Watsonstreet 12')->city('Bot City')->postalCode(100000)->state('Washington AI')->country('Botmanland'))
+            ->addSummary(ReceiptSummary::create()->subtotal(18.98)->shippingCost(10)->totalTax(15)->totalCost(23.98))
+            ->addAdjustment(ReceiptAdjustment::create('Laravel Bonus')->amount(5));
+
+        $payload = $driver->buildServicePayload($template, $message);
+        $driver->sendPayload($payload);
+        $driver->messagesHandled();
+
+        $json = $template->toWebDriver();
+        $this->expectOutputString('{"status":200,"messages":['.json_encode($json).']}');
+    }
+}
